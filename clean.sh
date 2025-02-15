dd81d66574ae3e2dba429d97d3a8bd1707fe2829
Jeroen Bernsen <>
2025-02-15 20:09:00 +0100

    Improved clean script

--- clean.sh
+++ clean.sh
@@ -3,5 +3,6 @@ find . -name 'obj' -exec rm -rf {} \;
 find . -name '.idea' -exec rm -rf {} \;
 find . -name '.vs' -exec rm -rf {} \;
 
-rm -rf XCFrameworks/*
+rm -rf WidgetCenterProxy/build
 rm -rf WidgetCenterProxyBinder/WidgetCenterProxyBinder/nugetoutput
+rm -rf XCFrameworks/*
